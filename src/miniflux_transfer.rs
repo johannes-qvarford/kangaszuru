ef6fc42200fad860c0289cb53154ae574a29d8eb
Johannes Qvarford <jq.email+github@pm.me>
2023-04-02 23:29:44 +0200

    Removing debug logging.

--- src/miniflux_transfer.rs
+++ src/miniflux_transfer.rs
@@ -4,10 +4,8 @@ pub(crate) fn perform(miniflux_context: MinifluxContext, szurubooru_context: Szu
     let posts = miniflux_context.download_starred_posts();
 
     let media_links: Vec<_> = posts.iter().flat_map(|post| post.media_links()).collect();
-    println!("Media links are: {media_links:?}");
 
     let new_media_links: Vec<_> = media_links.into_iter().filter(|link| !szurubooru_context.post_is_already_uploaded(link)).collect();
-    println!("New Media links are: {new_media_links:?}");
 
     for link in new_media_links {
         szurubooru_context.upload_post(link);
